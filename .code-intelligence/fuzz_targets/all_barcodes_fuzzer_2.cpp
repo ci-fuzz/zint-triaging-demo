ca9de3f7d810d0ecf80a6624b37600472457f711
wojtek <dziewiecki@code-intelligence.com>
2022-05-19 13:36:05 +0200

    Fuzz with correct rotate_angle values to cover code after an ASSERT statement

--- .code-intelligence/fuzz_targets/all_barcodes_fuzzer_2.cpp
+++ .code-intelligence/fuzz_targets/all_barcodes_fuzzer_2.cpp
@@ -128,7 +128,7 @@ extern "C" int FUZZ(const unsigned char *Data, size_t Size)
 
   int fuzzed_int = dp.ConsumeIntegral<int>();
   int fuzzed_int2 = dp.ConsumeIntegral<int>();
-  int fuzzed_int3 = dp.ConsumeIntegral<int>(); // unused
+  int fuzzed_int3 = dp.ConsumeIntegral<int>();
   int fuzzed_symbology = dp.PickValueInArray(BARCODES);
   bool fuzzed_bool = dp.ConsumeBool();
   bool fuzzed_bool2 = dp.ConsumeBool();
@@ -136,6 +136,7 @@ extern "C" int FUZZ(const unsigned char *Data, size_t Size)
   int rotate_angle = dp.ConsumeIntegral<int>();
   auto remaining = dp.ConsumeRemainingBytes<unsigned char>();
 
+  if (fuzzed_bool3) rotate_angle = (rotate_angle % 4) * 90; // ZBarcode_Print exits if angle is not a multiple of 90
 
   struct zint_symbol *my_symbol = ZBarcode_Create();
   if (fuzzed_bool) {  
@@ -151,6 +152,7 @@ extern "C" int FUZZ(const unsigned char *Data, size_t Size)
   ZBarcode_Encode_and_Buffer(my_symbol, remaining.data(), remaining.size(), rotate_angle);
   ZBarcode_Encode_and_Print(my_symbol, remaining.data(), remaining.size(), rotate_angle);
   //ZBarcode_Encode(my_symbol, remaining.data(), remaining.size());
+  ZBarcode_Clear(my_symbol);
   ZBarcode_Delete(my_symbol);
 
   return 0;
